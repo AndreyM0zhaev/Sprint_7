c137ffd2ed3b7bafc6ab04ab31a935f28e73dbb5
Andrey Mozhaev <Mozhaev.andrey.v@yandex.com>
2025-01-20 20:52:51 +0300

    add test_delete_courier

--- endpoints.py
+++ endpoints.py
@@ -3,3 +3,4 @@ class Endpoint:
     CREATE_COURIER = '/api/v1/courier'
     LOGIN_COURIER = '/api/v1/courier/login'
     CREATE_ORDER = '/api/v1/orders'
+    DELETE_COURIER = '/api/v1/courier/'
--- tests/test_create_courier.py
+++ tests/test_create_courier.py
@@ -9,8 +9,11 @@ from data_user import registration_new_courier_without_login as new_reg_without_
 from data_user import registration_new_courier_without_password as new_reg_without_password
 
 
+
 class TestCreateCourier:
     data = new_reg()
+    courier_id = None
+
 
     @allure.title('Успешное создание учетной записи')
     @allure.description('Проверка, что курьер успешно создаётся при корректных данных')
@@ -24,6 +27,22 @@ class TestCreateCourier:
             assert response.status_code == 201
             assert response.json() == {"ok": True}
 
+        TestCreateCourier.courier_id = self.get_courier_id(TestCreateCourier.data['login'], TestCreateCourier.data['password'])
+
+    def get_courier_id(self, login, password):
+        with allure.step('Получение ID курьера'):
+            credentials = {
+                "login": login,
+                "password": password
+            }
+            response = requests.post(f'{Constants.BASE_URL}{Endpoint.LOGIN_COURIER}', json=credentials)
+            allure.attach(f'Запрос: {credentials}', name='Request Body', attachment_type=allure.attachment_type.TEXT)
+            allure.attach(f'Ответ: {response.text}', name='Response Body', attachment_type=allure.attachment_type.TEXT)
+
+            assert response.status_code == 200
+            return response.json()['id']
+
+
     @allure.title('Запрос с повторяющимся логином')
     @allure.description('Проверка, что нельзя создать курьера с уже существующим логином')
     def test_create_courier_duplicate(self):
@@ -35,6 +54,7 @@ class TestCreateCourier:
         with allure.step('Проверка кода ответа и сообщения об ошибке'):
             assert response.status_code == 409
             assert 'Этот логин уже используется' in response.text
+
 
     @allure.title('Запрос создания курьера без логина')
     @allure.description('Проверка, что нельзя создать курьера без указания логина')
@@ -49,6 +69,7 @@ class TestCreateCourier:
             assert response.status_code == 400
             assert 'Недостаточно данных для создания учетной записи' in response.text
 
+
     @allure.title('Запрос создания курьера без пароля')
     @allure.description('Проверка, что нельзя создать курьера без указания пароля')
     def test_create_courier_without_password(self):
@@ -61,3 +82,20 @@ class TestCreateCourier:
         with allure.step('Проверка кода ответа и сообщения об ошибке'):
             assert response.status_code == 400
             assert 'Недостаточно данных для создания учетной записи' in response.text
+
+
+    @allure.title('Удаление курьера')
+    @allure.description('Проверка, что курьер успешно удаляется')
+    def test_delete_courier(self):
+        if TestCreateCourier.courier_id is not None:
+            with allure.step('Удаление курьера'):
+                response = requests.delete(f'{Constants.BASE_URL}{Endpoint.DELETE_COURIER}/{TestCreateCourier.courier_id}')
+                allure.attach(f'Запрос: DELETE {Endpoint.DELETE_COURIER}/{TestCreateCourier.courier_id}', name='Request', attachment_type=allure.attachment_type.TEXT)
+                allure.attach(f'Ответ: {response.text}', name='Response Body', attachment_type=allure.attachment_type.TEXT)
+
+            with allure.step('Проверка кода ответа и тела ответа'):
+                assert response.status_code == 200
+                assert response.json() == {"ok": True}
+        else:
+            allure.attach('Курьер не был создан, удаление невозможно', name='Info', attachment_type=allure.attachment_type.TEXT)
+            assert False, "Курьер не был создан, удаление невозможно"
